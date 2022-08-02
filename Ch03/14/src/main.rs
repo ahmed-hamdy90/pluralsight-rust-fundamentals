4070a4b9d0e69252c6fc95e05e8f62bb0dab09be
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2022-08-03 00:08:35 +0200

    Fix some spilling mistake on comment and add new ones

--- Ch03/14/src/main.rs
+++ Ch03/14/src/main.rs
@@ -6,15 +6,15 @@ fn main() {
     let duck = "Duck";
     let airlines = "Airlines";
 
-    // Then concat them with different ways (Notice: all concatination result give String variables from Strings Slices)
+    // Then concat them with different ways (Notice: all concatenation result give String variables from Strings Slices)
     let airlines_name_1 = [duck, " ", airlines].concat();
     println!("{}", airlines_name_1);
     let airlines_name_2 = format!("{}, {}", duck, airlines);
 
     // There another way for create String and begin edit it to works as concat Strings Slices
     let mut slogan = String::new();
-    slogan.push_str("We hit the ground");
-    slogan.push(' ');
+    slogan.push_str("We hit the ground"); // add a string
+    slogan.push(' '); // add character
     slogan = slogan + "every time";
 
     println!("{}", slogan)
